fd7aa8197e788fe72061b1c828f185b7c86e4c87
AlexPads <37447608+AlexPads@users.noreply.github.com>
2018-10-03 00:15:34 -0400

    Add files via upload

---
+++ src/TinyPixelDevz/Main.php
@@ -0,0 +1,54 @@
+<?php
+namespace TinyPixelDevz;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDamageEvent;
+class Main extends PluginBase implements Listener{
+    
+    public $iswildin = [];
+    
+    public function onEnable(){
+              $this->getServer()->getPluginManager()->registerEvents($this, $this);
+              $this->getLogger()->info(TF::GREEN . "Wild enabled!");
+    }
+    public function onDisable(){
+              $this->getLogger()->info(TF::RED . "Wild disabled!");
+    }
+    
+    public function onCommand(CommandSender $s, Command $cmd, string $label, array $args) : bool{
+    if(strtolower($cmd->getName() == "wild")){
+        if($s instanceof Player){
+            $x = rand(1,999);
+            $y = 128;
+            $z = rand(1,999);
+		$level = Server::getInstance()->getConfig()->getNested("World");
+		//LINE 32 needs to be checkd that what the error is should be easy im just tired xD
+            $s->teleport($s->getLevel()->getSafeSpawn(new Vector3($x, $y, $z, $level)));
+            $s->addTitle->getConfig()->getNested("Name");
+	    $s->sendMessage->getConfig()->getNested("Text");
+            $this->iswildin[$s->getName()] = true;
+        
+        }
+        }else{
+            $s->sendMessage(TF::RED."You dont have permission");
+        }
+        return true;
+    } 
+    public function onDamage(EntityDamageEvent $event){
+       if($event->getEntity() instanceof Player){
+           if(isset($this->iswildin[$event->getEntity()->getName()])){
+               $p = $event->getEntity();
+               unset($this->iswildin[$p->getName()]);
+                     $event->setCancelled();
+           }
+       }
+    }
+}
